50847b98896c6bbcc0927b28e9455cf4585c3088
newmin0119@gmail.com <>
2023-08-25 22:51:16 +0900

    [feat] 로그인 회원가입 기능 추가

---
+++ accounts/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import User
+
+class UserSignUpSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['email'],validated_data['password'],
+            **{
+                'nick_name': validated_data['nick_name'],
+            })
+        user.save()
+        return user
+
+class UserModelSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email','nick_name']
