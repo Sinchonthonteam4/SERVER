45e366977fced59ac0bfb209cf621c84c516a3c5
xecond <ksw09203@gmail.com>
2023-08-25 21:05:40 +0900

    [feat]

---
+++ communities/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+#
+
+# class Post(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE,  null=True, blank=True)
+#     title = models.CharField(max_length=100)
+#     content = models.TextField()
+#     create_at = models.DateTimeField(auto_now_add=True)
+#     category = 
+    
--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'reports',
+    'communities',
 ]
 
 MIDDLEWARE = [
