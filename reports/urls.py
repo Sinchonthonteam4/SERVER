f3344c154bf8cc564385199b4895cb75db331f43
xecond <ksw09203@gmail.com>
2023-08-26 03:24:34 +0900

    [fix] cafes DB 수정

--- reports/urls.py
+++ reports/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', DailyReportCreateListView.as_view()),
     path('daily/',DailyReportAPIView.as_view()),
     path('<int:pk>',DailyReportRetrieveDestroyView.as_view()),
-    path('challenge/', ListAPIView.as_view()),
 ]
